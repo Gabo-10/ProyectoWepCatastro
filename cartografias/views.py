3937d3a46bc4155fb092df0ffa0c8c293bf5087a
EduardoGallardoR <egallardo821@gmail.com>
2024-06-10 12:12:39 -0600

    cartografia

--- cartografias/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from cartografias.models import Cartografia
-
-# Create your views here.
-
-def cartografias(request):
-    
-    cartografias=Cartografia.objects.all()
-    return render(request, "cartografias/cartografias.html", {"cartografias": cartografias})
