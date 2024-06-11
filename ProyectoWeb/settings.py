82c14eac4f245344eb7da6d43af2010a1d666cc4
gabriel <gabrielm1877@gmail.com>
2024-06-10 21:39:19 -0600

    Creación de DB en pgAdmin4

--- ProyectoWeb/settings.py
+++ ProyectoWeb/settings.py
@@ -84,9 +84,18 @@ WSGI_APPLICATION = 'ProyectoWeb.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'catastro',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'DATABASE_PORT': '5432',
+        
     }
 }
 
