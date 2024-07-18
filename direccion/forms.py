2296a5de40cea27fbc55ec7b53f36727a5b4edcc
EduardoGallardoR <egallardo821@gmail.com>
2024-07-18 14:10:34 -0600

    ayuda

--- direccion/forms.py
+++ direccion/forms.py
@@ -2,8 +2,19 @@ from django import forms
 
 class FormularioDireccion(forms.Form):
     
-    nombre=forms.CharField(label='Nombre', required=True)
-    email=forms.CharField(label='Email', required=True)
-    contenido=forms.CharField(label='Contenido', widget=forms.Textarea)
+    nombre = forms.CharField(
+        label='Nombre', 
+        required=True, 
+        widget=forms.TextInput(attrs={'class': 'custom-input'})
+    )
+    email = forms.EmailField(
+        label='Email', 
+        required=True, 
+        widget=forms.EmailInput(attrs={'class': 'custom-input'})
+    )
+    contenido = forms.CharField(
+        label='Contenido', 
+        widget=forms.Textarea(attrs={'class': 'custom-textarea'})
+    )
     
   
