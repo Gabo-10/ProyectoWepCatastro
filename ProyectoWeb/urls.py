6fc51505adc20cca1bce19fb102eea7ecf1dea98
EduardoGallardoR <egallardo821@gmail.com>
2024-07-19 13:53:40 -0600

    inspeccion

--- ProyectoWeb/urls.py
+++ ProyectoWeb/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
@@ -40,3 +44,8 @@ urlpatterns = [
     path('inicial/', include('ProyectoWebApp.urls')),# para que directamente se dirija a esta url sin nececidad de cololar ProyectoWebApp/
    
 ]
+# Configuración para servir archivos estáticos en desarrollo
+urlpatterns += staticfiles_urlpatterns()
+
+# Configuración para servir archivos multimedia en desarrollo
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
